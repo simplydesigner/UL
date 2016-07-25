8300d53f6939cacbdb0be1d05df647d6524ebb91
simplydesigner <simplydesigner@ya.ru>
2016-07-25 12:02:57 +0300

    Initial Commit

---
+++ UniversalLink/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UniversalLink
+//
+//  Created by simplydesigner on 7/25/16.
+//  Copyright © 2016 simplydesigner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
